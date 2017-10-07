ecb29d84b02f96a15a10fdb2a6d0eed2685a7feb
Bruno Cavallaro <bfcvll@gmail.com>
2017-10-07 19:33:34 -0300

    Fix test arguments parsing

--- cucumber/features/steps/sum_test.rb
+++ cucumber/features/steps/sum_test.rb
@@ -1,11 +1,11 @@
 require 'minitest/autorun'
 
 Given(/^I have (\d+) apples$/) do |arg1|
-  @cur_apples = arg1
+  @cur_apples = arg1.to_i
 end
 
 When(/^I buy (\d+) more apples$/) do |arg1|
-  @new_apples = arg1
+  @new_apples = arg1.to_i
 end
 
 Then(/^I have a total of (\d+) apples to eat$/) do |arg1|
